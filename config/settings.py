ae4b255a9a32521ad26adc28fd06f6b2d0029cb5
Gurrala Shivani <shivanireddygurrala08@gmail.com>
2026-04-29 18:16:37 +0530

    Update AI Code Reviewer project structure

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ai_code_reviewer.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -52,7 +52,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'ai_code_reviewer.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 DATABASES = {
